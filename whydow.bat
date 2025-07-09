1ca519f1338ca9a56fc630ef3356875f24b99854
T_cat9000_2 <terpstragraham@gmail.com>
2025-07-09 13:26:58 -0700

    del

--- whydow.bat
+++
@@ -1,2 +0,0 @@
-powershell.exe Invoke-WebRequest https://raw.githubusercontent.com/webbrowser11/whydows/main/whydows.bat -OutFile C:\Windows\Temp\whydows.bat
-powershell.exe -Command "Start-Process 'C:\Windows\Temp\whydows.bat'"
